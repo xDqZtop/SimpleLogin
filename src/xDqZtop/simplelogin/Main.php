28a87f7350efac2504b54425b59e7700b7c5b907
xDqZtop <xdqztop@gmail.com>
2025-03-30 08:10:02 +0330

    Update Main.php
    idk

--- src/xDqZtop/simplelogin/Main.php
+++ src/xDqZtop/simplelogin/Main.php
@@ -1,1 +1,41 @@
+<?php
 
+declare(strict_types=1);
+
+namespace xDqZtop\simplelogin;
+
+use pocketmine\plugin\PluginBase;
+use Throwable;
+
+class Main extends PluginBase {
+
+    public static ?Main $instance = null;
+    public DataManager $dataManager;
+
+    public function onLoad(): void {
+        self::$instance = $this;
+    }
+
+    public function onEnable(): void {
+        self::$instance = $this;
+        $logger = $this->getLogger();
+
+        try {
+            $this->dataManager = new DataManager();
+            $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
+            $logger->notice("Plugin enabled successfully!");
+        } catch (Throwable $e) {
+            $logger->error("Enable error: " . $e->getMessage());
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+        }
+    }
+
+    public function onDisable(): void {
+        $logger = $this->getLogger();
+        $logger->notice("Disabled!");
+    }
+
+    public function getDataManager(): DataManager {
+        return $this->dataManager;
+    }
+}
